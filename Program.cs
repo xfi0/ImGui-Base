13ced6614354d68076b2b3a35e5f11e9558f987d
dom <166765848+xfi0@users.noreply.github.com>
2026-05-18 16:45:19 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+using ImGui_Base;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Renderer renderer = new();
+        Thread renderThread = new(() => renderer.Start().Wait())
+        {
+            IsBackground = true,
+        };
+
+        renderThread.Start();
+
+        while (true)
+        {
+            Thread.Sleep(1);
+        }
+    }
+}
---
+++ Renderer.cs
@@ -0,0 +1,26 @@
+﻿using ClickableTransparentOverlay;
+using ImGuiNET;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace ImGui_Base
+{
+    internal class Renderer : Overlay
+    {
+        private static Vector2 windowSize = new(800, 400);
+
+        protected override void Render()
+        {
+            var screenSize = ImGui.GetIO().DisplaySize;
+
+            ImGui.SetNextWindowSize(windowSize, ImGuiCond.FirstUseEver);
+            ImGui.SetNextWindowPos(new Vector2((screenSize.X / 2) - windowSize.X, (screenSize.Y / 2) - windowSize.Y), ImGuiCond.FirstUseEver);
+            ImGui.Begin("Base");
+            ImGui.Text("Please star this if you used this base!");
+            ImGui.Text("Put all widgets in between begin and end.");
+            ImGui.End();
+        }
+    }
+}
